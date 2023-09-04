39b70dcc576321472594fef0651691950373ff1b
TL <ncdongt@gmail.com>
2023-09-04 16:03:36 +0700

    second try

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div>Xin Chao, ten toi la Do Thanh Liem</div>
-      <div className="bg-cyan-200"> Xin Chao, ten toi la Do Thanh Liem</div>
+      <div className="bg-cyan-200"> Hello, ten toi la Do Thanh Liem</div>
        <img className="w-24 rounded mt-4" src="/abc.jpg" />
     </main>
   )
