fb8c2a2a5e26e88b7290ad361dec013c2f7a7af4
PC TL <22520751@gm.uit.edu.vn>
2023-09-04 20:55:06 +0700

    Change Background, Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ const inter = Inter({ subsets: ['latin'] });
 const Home = () => {
   return (
     <main className={inter.className}>
-      <div> Xin Chao, ten toi la Do Thanh Liem </div>
+      <div> Welcome to my website </div>
       <div className="bg-cyan-200"> Hello, my name is Do Thanh Liem </div>
-      <img className="w-24 rounded mt-4" src="/abc.jpg" alt="Avatar" />
+      <img className="w-128 rounded" src="/Background.jpg" alt="Background" />
     </main>
   );
 };
