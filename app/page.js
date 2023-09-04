5a15fa69108a30552bc0512c420717f6cd8b78b2
TL <ncdongt@gmail.com>
2023-09-04 15:58:36 +0700

    first try

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <div>Xin Chao, ten toi la Do Thanh Liem</div>
+      <div className="bg-cyan-200"> Xin Chao, ten toi la Do Thanh Liem</div>
+       <img className="w-24 rounded mt-4" src="/abc.jpg" />
     </main>
   )
 }
