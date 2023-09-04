ce2c843b99a96c74e3926f4f6e6e9d7d18a0e4c6
PC TL <22520751@gm.uit.edu.vn>
2023-09-04 20:36:06 +0700

    3rd try

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ const inter = Inter({ subsets: ['latin'] });
 const Home = () => {
   return (
     <main className={inter.className}>
-      <div>Xin Chao, ten toi la Do Thanh Liem</div>
-      <div className="bg-cyan-200"> Hello, ten toi la Do Thanh Liem</div>
+      <div> Xin Chao, ten toi la Do Thanh Liem </div>
+      <div className="bg-cyan-200"> Hello, my name is Do Thanh Liem </div>
       <img className="w-24 rounded mt-4" src="/abc.jpg" alt="Avatar" />
     </main>
   );
