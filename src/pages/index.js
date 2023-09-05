87da23ea70569aae5aef8f8c768e988776d922c3
TL <ncdongt@gmail.com>
2023-09-05 17:01:58 +0700

    Bold name

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <main className={inter.className}>
       <div> Welcome to my website </div>
-      <div className="bg-cyan-200"> Hello, my name is Do Thanh Liem </div>
+      <div className="bg-cyan-200"> Hello, my name is <b>  Do Thanh Liem </b> </div>
       <img className="w-24 rounded" src="/Ava.jpg" alt="Background" />
       <img className="w-128 rounded" src="/Background.jpg" alt="Background" />
     </main>
