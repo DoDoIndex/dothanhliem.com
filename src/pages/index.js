cbc5b7750c63b4f3554477cfa33f80a9dfeab954
PC TL <22520751@gm.uit.edu.vn>
2023-09-06 15:00:22 +0700

    conflict fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,7 @@ const Home = () => {
     <main className={inter.className}>
       <div> Welcome to my website </div>
       <div className="bg-cyan-200">
-        {' '}
-        Hello, my name is <b> Do Thanh Liem </b>{' '}
+        Hello, my name is <b> Do Thanh Liem </b>
       </div>
       <img className="w-24 rounded" src="/Ava.jpg" alt="Background" />
       <img className="w-128 rounded" src="/Background.jpg" alt="Background" />
