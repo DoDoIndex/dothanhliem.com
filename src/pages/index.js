4642658a90a25e129bba0620d6e960c51eade0f0
PC TL <22520751@gm.uit.edu.vn>
2023-09-07 15:19:58 +0700

    update 07-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,12 @@ const inter = Inter({ subsets: ['latin'] });
 const Home = () => {
   return (
     <main className={inter.className}>
+      <title>Profile - Thanhliem Do</title>
       <div> Welcome to my website </div>
       <div className="bg-cyan-200">
-        Hello, my name is <b> Do Thanh Liem </b>
+        Hello, my name is <b> Do Thanh Liem </b> <br></br>
+        <a href="https://www.facebook.com/tl.do.940436"> My Facebook </a>
       </div>
-      <a href="https://www.facebook.com/tl.do.940436"> My Facebook </a>
       <img className="w-24 rounded" src="/Ava.jpg" alt="Background" />
       <img className="w-128 rounded" src="/Background.jpg" alt="Background" />
     </main>
