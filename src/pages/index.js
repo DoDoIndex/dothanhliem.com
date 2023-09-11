0035a6ef2396a93e17a1bb8e0ee1868460ebde88
PC TL <22520751@gm.uit.edu.vn>
2023-09-11 15:46:00 +0700

    Add some css

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,9 @@ const Home = () => {
         Hello, my name is <b> Do Thanh Liem </b> <br></br>
         <a href="https://www.facebook.com/tl.do.940436"> My Facebook </a>
       </div>
+      <button id="b1">Button 1</button>
+      <br></br>
+      <button id="b2">Button 2</button>
       <img className="w-28 rounded" src="/Ava.jpg" alt="Background" />
       <img className="w-128 rounded" src="/Background.jpg" alt="Background" />
     </main>
