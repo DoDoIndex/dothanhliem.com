c26d5e51f637297257943a5363ff76068713f21a
PC TL <22520751@gm.uit.edu.vn>
2023-09-24 10:59:18 +0700

    Fixed and Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,42 @@ const Home = () => {
   return (
     <main className={inter.className}>
       <title>Profile - Thanhliem Do</title>
-      <div> Welcome to my website </div>
-      <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZbjLpWTNGQvTtXRjHDCzLCwpLzBfJZnTrLmFqfzdxFghxwdnxLnqTFLwWqqpjBMQVVlkg">
-        {' '}
-        225210751@gm.uit.edu.vn
-      </a>
-      <div className="bg-cyan-200">
-        Hello, my name is <b> Do Thanh Liem </b> <br></br>
-        <a href="https://www.facebook.com/tl.do.940436">My Facebook</a>
-      </div>
-      <button id="b1">Button 1</button>
-      <br></br>
-      <button id="b2">Button 2</button>
-      <img className="w-28 rounded" src="/Ava.jpg" alt="Background" />
+
+      <header>
+        <section class="Ava">Ava is not yet added</section>
+
+        <section class="about">
+          <h1 id="h1_1">About Me:</h1>
+
+          <div id="name">Full Name: Do Thanh Liem</div>
+          <div id="bd">Birthday: 20-12-2004</div>
+        </section>
+
+        <section class="contact">
+          <h1 id="h1_2">Contact:</h1>
+
+          <div id="phone">Phone: 0816494092</div>
+          <div id="email">
+            Email:{' '}
+            <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZdBnsMcxdkzdjLpDXRBPTBSBKnxrSwLKRdGxFZzNqbtchNZDtRtNcJXkCfnsKrtzdsxRL">
+              ncdongt@gmail.com
+            </a>
+          </div>
+          <div id="fb">
+            Facebook: <a href="https://www.facebook.com/tl.do.940436">Thanhliem Do</a>
+          </div>
+          <div id="addr">Address: Phan Van Tri St, Binh Thanh, Ho Chi Minh</div>
+        </section>
+
+        <section class="Education">
+          <h1 id="h1_3">Education</h1>
+          <div id="high">High School: Gia Dinh senior high school - Ho Chi Minh</div>
+          <div id="uni">University: University of Infomation Technology - Ho Chi Minh National University</div>
+        </section>
+      </header>
+
+      <section class="skills"></section>
+
       <img className="w-128 rounded" src="/Background.jpg" alt="Background" />
     </main>
   );
